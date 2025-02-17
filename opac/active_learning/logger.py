cc98f604f0d217bd94bd0fd95cc4c9982a7b0187
Osaro, 'Nosa James <64130121+theOsaroJ@users.noreply.github.com>
2025-02-17 11:52:41 -0500

    Create logger.py

---
+++ opac/active_learning/logger.py
@@ -0,0 +1,12 @@
+import logging
+
+def get_logger(name):
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        # Prevent adding multiple handlers to the logger
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s %(levelname)s:%(name)s: %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.setLevel(logging.INFO)
+    return logger
